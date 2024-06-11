e110da14e47e82bd6f7283ceeacbb4fff5f374b0
ia <iagugunava05@gmail.com>
2024-06-12 03:21:36 +0400

    wip: swipers and swiper cards

---
+++ components/SwiperCard.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+/**
+ * @author Ia Gugunava
+ */
+import StarsRating from './UiComponents/StarsRating.vue';
+
+const data = {
+    img: "https://movies-proxy.vercel.app/ipx/f_webp&s_400x600/tmdb/rXojaQcxVUubPLSrFV8PD4xdjrs.jpg",
+    title: "hazbin hotel",
+    rating: 9
+}
+</script>
+
+<template>
+    <div class="swiper-card">
+        <div class="wrapper">
+            <img class="swiper-card__image" :src="data.img"/>
+        </div>
+
+        <div class="swiper-card__title">
+            {{ data.title }}
+        </div>
+
+        <div class="swiper-card__rating">
+            <StarsRating :value="data.rating"/>
+
+            <p>{{ data.rating }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.swiper-card{
+    .wrapper{
+        position: relative;
+        height: auto;
+        padding-top: calc(100% / (232 / 376));
+        overflow: hidden;
+    }
+    &__image{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+        transition: all 500ms ease-in-out;
+    }
+    &__title{
+        color: white;
+        font-size: 21px;
+        margin-top: 20px;
+    }
+    &__rating{
+        display: flex;
+        gap: 16px;
+        align-items: center;
+
+        p{
+            color: white;
+            font-size: 16px;
+        }
+    }
+
+    &:hover{
+        .swiper-card__image{
+            transform: scale(1.2);
+        }
+    }
+}
+</style>
---
+++ components/UiComponents/StarsRating.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+/**
+ * @author Ia Gugunava
+ */
+const props = withDefaults(
+  defineProps<{
+    value?: number
+  }>(),
+  {
+    value: 0,
+  },
+)
+
+const style = computed(() => ({
+  'clip-path': `inset(0 ${(10 - props.value) * 10}% 0 0)`,
+}))
+</script>
+
+<template>
+  <div class="stars-rating">
+    <img class="stars-rating__stars" src="/stars.webp" />
+    <img class="stars-rating__stars" src="/stars-filled.webp" :style="style"/>
+  </div>
+</template>
+
+<style lang="scss">
+.stars-rating{
+    position: relative;
+    width: 5rem;
+    aspect-ratio: 11 / 2;
+
+    &__stars{
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height: auto;
+        max-width: 100%;
+    }
+}
+</style>
