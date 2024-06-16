b9eb067c6ba424dee65c1769c24a43c2e5d1aefc
ia <iagugunava05@gmail.com>
2024-06-17 02:16:42 +0400

    wip: project configuration

--- app.vue
+++ app.vue
@@ -9,5 +9,7 @@
 <style lang="scss">
 body {
   background-color: #111;
+  background-image: url("./assets/images/image.png");
+  background-size: contain;
 }
 </style>
---
+++ components/PopularsSwiper.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import SwiperCard from './UiComponents/SwiperCard.vue';
+
+/**
+ * @author Ia Gugunava
+ */
+</script>
+
+<template>
+    <div class="populars-swiper">
+        <div class="container">
+            <h3 class="populars-swiper__title">popular movies</h3>
+            <Swiper
+            :modules="[SwiperNavigation]"
+            :slides-per-view="5"
+            :space-between="50"
+            >
+            <SwiperSlide v-for="slide in 10" :key="slide">
+                <SwiperCard/>
+            </SwiperSlide>
+            </Swiper>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.populars-swiper{
+    margin: 70px 0;
+    &__title{
+        // color: ;
+        font-size: 44px;
+    }
+
+    .swiper{
+        overflow: visible;
+    }
+}
+</style>
